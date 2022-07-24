12b50dc218d06580b35bca9bc21d20f8905b0479
Ishita Johri <73711981+IshitaJohri@users.noreply.github.com>
2022-07-24 21:02:11 +0530

    Add files via upload

---
+++ EvenOrOdd (1).py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+# st.title('Multiplication of 2 numbers')
+# a = st.number_input('Enter a number')
+# b = st.number_input('Enter another number')
+# result = a * b
+# st.write(a, ' * ', b , '= ', result)
+
+st.title('Finding whether the given number is odd or even')
+a = st.number_input('Enter a number')
+if(a%2==0):
+  result = "even number"
+else :
+  result = "odd number"
+st.write(a, ' is an', result)
